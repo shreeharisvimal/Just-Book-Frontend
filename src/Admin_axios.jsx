9dc61c33488f2a2f09bf7e4666e5cd960b121292
shreeharisvimal <ssvambalapuzha@gmail.com>
2024-07-19 11:43:26 +0530

    .env config

--- src/Admin_axios.jsx
+++ src/Admin_axios.jsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 const accessToken = localStorage.getItem("AccessToken");
 
 const Instance = axios.create({
-  baseURL: 'http://127.0.0.1:8000/',
+  baseURL: process.env.REACT_APP_BACKEND_URL,
   headers: {
     Authorization: `Bearer ${accessToken}`
   }
